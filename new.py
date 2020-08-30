139cb76be5967884b1b47fc544e626b3d099822e
Hema k <Hema@gmail.com>
2020-08-30 05:20:02 +0000

    updated code

--- new.py
+++ new.py
@@ -4,3 +4,8 @@ this is the third line of code
 this is the fourth line of code
 This is mfy another line of code
 hello sumit
+this is the correct code which i update and it will be working fine
+if all will implete same in the produ
+this is kind of new impleteation we have.
+earlir job was failing due soem code issue
+
