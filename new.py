ab504c2499c116bff9881c1e605bc85a3ba0f56c
Hema k <Hema@gmail.com>
2020-09-01 02:08:27 +0000

    updated file

--- new.py
+++ new.py
@@ -9,5 +9,5 @@ this is the correct code which i update and it will be working fine
 if all will implete same in the produ
 this is kind of new impleteation we have.
 earlir job was failing due soem code issue
-
+Updatd JIRA tcket
 I have update JIRA ticket and it is working fine
