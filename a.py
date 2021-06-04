93510f7bf65d462bfcd475073fd1a5225b4e8242
dkshahi <49384708+dkshahi@users.noreply.github.com>
2021-06-04 21:17:59 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,3 +1,3 @@
-print("THis is my new batch which i started last week")
-print("This is updated line of code")
+print1("THis is my new batch which i started last week")
+print1("This is updated line of code")
 
